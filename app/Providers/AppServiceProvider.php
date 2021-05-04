d210fbc86b36d1b98eb31ed62eb1c0d70bab5878
Éverton <foreverplayer95@gmail.com>
2021-05-04 18:21:46 -0300

    atualizações

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider{
     public function boot(UrlGenerator $url)
     {
 
-        $url->forceScheme('httpS');
+        $url->forceScheme('https');
 
      }
     /**
