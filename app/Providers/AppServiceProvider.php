e6eb7b15354b852134820acf47f63b751e55ad24
Éverton <foreverplayer95@gmail.com>
2021-05-04 17:44:17 -0300

    atualizações

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,10 @@ class AppServiceProvider extends ServiceProvider{
      */
     public function boot(UrlGenerator $url)
     {
-            if (env('APP_ENV') != 'local') {
+            if (env('APP_ENV') == 'local') {
                   $url->forceScheme('http');
+            }else{
+                $url->forceScheme('http');
             }
      }
     /**
