6d80ebd104731d1e55480256ca598be4b221a22c
Éverton <foreverplayer95@gmail.com>
2021-05-15 21:21:10 -0300

    redirect

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider{
     public function boot(UrlGenerator $url)
     {
 
-        $url->forceScheme('http');
+        $url->forceScheme('https');
 
      }
     /**
